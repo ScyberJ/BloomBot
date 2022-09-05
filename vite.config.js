b10168fbd4a5be332f80f122458263fc3349c4ed
Aiden.J <kiddow12345@gmail.com>
2022-09-05 12:31:16 +0200

    Clean up css a bit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/BloomBot/',
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      watch: {
+        chokidar: false
+      }
+    }
+  }
 })
